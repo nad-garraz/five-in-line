f4b4f34d3877e9b5696440ab71dffae68842e898
Nad <nad.neutron@proton.me>
2022-06-21 02:08:17 +0200

    Added files: - welcomeScreen.cpp - welcomeScreen.h
    Fixed:
    - Gameplay winning detection.
    
    Misc:
    - Board top decoration and colors.

---
+++ welcomeScreen.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "welcomeScreen.h"
+
+using namespace std;
+
+void print_title() {
+  cout << "\x1B[31m        __  __     _             _ _                \033[0m"  << endl;
+  cout << "\x1B[31m        \\ \\/ /    (_)_ __       | (_)_ __   ___   \033[0m" << endl;
+  cout << "\x1B[31m         \\  /_____| | '_ \\ _____| | | '_ \\ / _ \\   \033[0m" << endl; 
+  cout << "\x1B[31m         /  \\_____| | | | |_____| | | | | |  __/   \033[0m" << endl;
+  cout << "\x1B[31m        /_/\\_\\    |_|_| |_|     |_|_|_| |_|\\___|  \033[0m" << endl;
+  }
+
+void print_menu(){
+cout << "        TODO menu" << endl;
+}
---
+++ welcomeScreen.h
@@ -0,0 +1,8 @@
+#ifndef WELCOMESCREEN_H
+#define WELCOMESCREEN_H
+
+
+void print_title();
+void print_menu();
+
+#endif
